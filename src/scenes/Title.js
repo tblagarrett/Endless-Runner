283f2b53035842299ebdad21bc965b9b5bf28a0f
tblagarrett <tblagarrett@gmail.com>
2024-02-04 02:04:47 -0800

    Changed continue to play on the title screen

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -5,7 +5,7 @@ class Title extends Phaser.Scene {
 
     create() {
         this.add.text(w/2, h/2, 'TITLE').setScale(2).setOrigin(0.5, 0.5)
-        this.add.text (w/2, h/2 + 50, 'Press space to continue').setScale(2).setOrigin(0.5, 0.5)
+        this.add.text (w/2, h/2 + 50, 'Press space to play').setScale(2).setOrigin(0.5, 0.5)
         this.add.text (w/2, h/2 + 100, 'Press shift for credits').setScale(2).setOrigin(0.5, 0.5)
     
         this.cursors = this.input.keyboard.createCursorKeys()
